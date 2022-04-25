c8f620b30fb392c28fa705e61e378340c480b95b
Lateef Quadri Olayinka <lateef9816@gmail.com>
2022-04-25 11:20:00 +0100

    adding files

---
+++ src/components/cart.tsx
@@ -0,0 +1,84 @@
+import {
+  AspectRatio,
+  Button,
+  Divider,
+  Heading,
+  HStack,
+  Image,
+  Stack,
+  Text,
+  useColorMode,
+  useColorModeValue,
+  VStack,
+} from "@chakra-ui/react";
+
+const Cart = () => {
+  const { toggleColorMode } = useColorMode();
+  const bgColor = useColorModeValue("gray.50", "whiteAlpha.50");
+  const secondaryTextColor = useColorModeValue("gray.600", "grey.400");
+  return (
+    <VStack
+      w="full"
+      h="full"
+      p={10}
+      spacing={10}
+      alignItems="flex-start"
+      bg={bgColor}
+    >
+      <VStack spacing={3} alignItems="flex-start">
+        <Heading size="2xl">Your Cart</Heading>
+        <Text>
+          If the price is too hard on your eyes,{" "}
+          <Button
+            onClick={toggleColorMode}
+            variant="link"
+            colorScheme={secondaryTextColor}
+          >
+            try changing the theme.
+          </Button>
+        </Text>
+      </VStack>
+      <HStack spacing={6} alignItems="center" w="full">
+        <AspectRatio ratio={1} w={24}>
+          <Image src="/images/skateboard.jpg" alt="a" />
+        </AspectRatio>
+        <Stack
+          spacing={0}
+          w="full"
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <VStack w="full" spacing={0} alignItems="center">
+            <Heading size="md">Penny Board</Heading>
+            <Text color={secondaryTextColor}>PNYCOMP27541</Text>
+          </VStack>
+          <Heading size="sm" textAlign="end">
+            $119.00
+          </Heading>
+        </Stack>
+      </HStack>
+      <VStack spacing={4} alignItems="stretch" w="full">
+        <HStack justifyContent="space-between">
+          <Text color={secondaryTextColor}>Subtotal</Text>
+          <Heading size="sm">$119.00</Heading>
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text color={secondaryTextColor}>Shipping</Text>
+          <Heading size="sm">$19.00</Heading>
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text color={secondaryTextColor}>Taxes (estimated)</Text>
+          <Heading size="sm">$23.00</Heading>
+        </HStack>
+      </VStack>
+      <Divider />
+      <HStack justifyContent="space-between" w="full">
+        <Text color={secondaryTextColor}>Total</Text>
+        <Heading size="lg">$162.79</Heading>
+      </HStack>
+    </VStack>
+  );
+};
+
+export default Cart;
---
+++ src/pages/home.tsx
@@ -0,0 +1,20 @@
+import { Container, Flex, VStack } from "@chakra-ui/react";
+import Cart from "../components/cart";
+import Details from "../components/details";
+
+function Home() {
+  return (
+    <Container maxW="container.xl" p={0}>
+      <Flex
+        h={{ base: "auto", md: "100vh" }}
+        py={[0, 10, 20]}
+        direction={{ base: "column-reverse", md: "row" }}
+      >
+        <Details />
+        <Cart />
+      </Flex>
+    </Container>
+  );
+}
+
+export default Home;
